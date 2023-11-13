520cf6dd19cfd5e7c91114b32bcb10925dab96a4
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2023-11-13 14:41:17 +0900

    Androidのビルドエラーを修正

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
