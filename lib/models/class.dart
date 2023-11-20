9d62b4d43e120f5f0060116c0c917fa99ffeec3c
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2023-11-20 14:44:39 +0900

    Classクラスを追加

---
+++ lib/models/class.dart
@@ -0,0 +1,15 @@
+import 'package:document_manager/models/user.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'class.freezed.dart';
+
+@freezed
+class Class with _$Class {
+  const factory Class({
+    required String id,
+    required String name,
+    required String description,
+    required User teacher,
+    required List<User> students,
+  }) = _Class;
+}
