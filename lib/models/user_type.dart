716eb52393c8380e887b9eecbad8ca2283e1488e
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2023-11-20 14:30:48 +0900

    列挙型UserTypeを追加

---
+++ lib/models/user_type.dart
@@ -0,0 +1,29 @@
+enum UserType {
+  teacher,
+  parent,
+  student,
+}
+
+extension UserTypeExtension on UserType {
+  String get text {
+    switch (this) {
+      case (UserType.teacher):
+        return 'teacher';
+      case (UserType.parent):
+        return 'parent';
+      case (UserType.student):
+        return 'student';
+    }
+  }
+
+  String get displayText {
+    switch (this) {
+      case (UserType.teacher):
+        return '教師';
+      case (UserType.parent):
+        return '保護者';
+      case (UserType.student):
+        return '学生';
+    }
+  }
+}
