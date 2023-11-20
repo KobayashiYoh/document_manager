8ee509e58a7aa6a7a0b1ec4098f08f3577f43c08
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2023-11-20 14:41:45 +0900

    Channelクラスを追加

---
+++ lib/models/channel.dart
@@ -0,0 +1,17 @@
+import 'package:document_manager/models/post.dart';
+import 'package:document_manager/models/user.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'channel.freezed.dart';
+
+@freezed
+class Channel with _$Channel {
+  const factory Channel({
+    required String id,
+    required String iconImageUrl,
+    required String name,
+    required String description,
+    required List<User> users,
+    required List<Post> posts,
+  }) = _Channel;
+}
