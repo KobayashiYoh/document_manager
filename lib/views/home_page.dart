68845702f6fad118c0fb5c54693fd47cdcc19169
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-01-11 17:58:42 +0900

    HomePageにチャンネル一覧を表示

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,32 +1,56 @@
+import 'package:document_manager/debug/debug_channel_item.dart';
+import 'package:document_manager/models/channel.dart';
+import 'package:document_manager/models/school.dart';
+import 'package:document_manager/repository/firestore_repository.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('home'),
+        title: Text(kExampleSchool.name),
       ),
-      body: ListView(
-        children: const [
-          ListTile(
-            title: Text('1-1'),
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.all(16.0),
+          child: Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              StreamBuilder(
+                stream: FirestoreRepository.channelSnapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.data == null) {
+                    return const SizedBox.shrink();
+                  }
+                  return ListView.builder(
+                    itemCount: snapshot.data!.docs.length,
+                    itemBuilder: (context, index) {
+                      final List<Channel> channels = snapshot.data!.docs
+                          .map((doc) => Channel.fromJson(doc.data()))
+                          .toList();
+                      return DebugChannelItem(channel: channels[index]);
+                    },
+                  );
+                },
+              ),
+            ],
           ),
-          Divider(),
-          ListTile(
-            title: Text('1-2'),
-          ),
-          Divider(),
-          ListTile(
-            title: Text('1-3'),
-          ),
-          Divider(),
-        ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          context.go('/debug');
+        },
+        child: const Icon(Icons.developer_mode),
       ),
     );
   }
