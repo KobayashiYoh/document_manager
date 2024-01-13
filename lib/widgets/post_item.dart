19d9b6eaf562f2bbb1cf8577346a1c0d7f70e915
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-01-13 10:31:28 +0900

    PostItemクラスを追加

---
+++ lib/widgets/post_item.dart
@@ -0,0 +1,43 @@
+import 'package:document_manager/models/post.dart';
+import 'package:document_manager/models/user.dart';
+import 'package:document_manager/widgets/circle_icon_image.dart';
+import 'package:flutter/material.dart';
+
+class PostItem extends StatelessWidget {
+  const PostItem({
+    Key? key,
+    required this.post,
+    required this.user,
+    required this.isMyPost,
+  }) : super(key: key);
+
+  final Post post;
+  final User user;
+  final bool isMyPost;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(bottom: 16.0),
+      child: Row(
+        children: [
+          CircleIconImage(
+            imageUrl: post.userId,
+            errorImagePath: '',
+          ),
+          const SizedBox(width: 8.0),
+          Flexible(
+            child: Container(
+              padding: const EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: Colors.green.shade100,
+                borderRadius: BorderRadius.circular(16.0),
+              ),
+              child: Text(post.message),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
