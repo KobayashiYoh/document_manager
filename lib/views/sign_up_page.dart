1568642bf8443a1b255c52da454d80f010a4ae5f
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-01-18 19:23:33 +0900

    キーボードに関する設定を変更

--- lib/views/sign_up_page.dart
+++ lib/views/sign_up_page.dart
@@ -63,99 +63,69 @@ class SignUpPageState extends ConsumerState<SignUpPage> {
     final state = ref.watch(signUpProvider);
     final notifier = ref.read(signUpProvider.notifier);
     final double keyboardHeight = MediaQuery.of(context).viewInsets.bottom;
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: const Text('新規ユーザー登録'),
-      ),
-      body: ListView(
-        padding: const EdgeInsets.all(32.0),
-        children: [
-          FormItem(
-            label: 'メールアドレス',
-            child: TextFormField(
-              controller: _emailController,
-              decoration: const InputDecoration(
-                hintText: 'email@example.com',
-              ),
-            ),
-          ),
-          const SizedBox(height: 32.0),
-          FormItem(
-            label: 'パスワード',
-            child: TextFormField(
-              controller: _passwordController,
-              obscureText: state.obscureText,
-              decoration: InputDecoration(
-                suffixIcon: PasswordSuffixIconButton(
-                  onPressed: notifier.switchObscureText,
-                  obscureText: state.obscureText,
+    return GestureDetector(
+      onTap: () => primaryFocus?.unfocus(),
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(
+          title: const Text('新規ユーザー登録'),
+        ),
+        body: ListView(
+          padding: const EdgeInsets.all(32.0),
+          children: [
+            FormItem(
+              label: 'メールアドレス',
+              child: TextFormField(
+                controller: _emailController,
+                keyboardType: TextInputType.emailAddress,
+                decoration: const InputDecoration(
+                  hintText: 'email@example.com',
                 ),
               ),
             ),
-          ),
-          const SizedBox(height: 32.0),
-          Row(
-            children: [
-              Expanded(
-                key: _lastNameKey,
-                child: FormItem(
-                  label: '苗字',
-                  child: TextFormField(
-                    controller: _lastNameController,
-                    decoration: const InputDecoration(hintText: '山田'),
+            const SizedBox(height: 32.0),
+            FormItem(
+              label: 'パスワード',
+              child: TextFormField(
+                controller: _passwordController,
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: state.obscureText,
+                decoration: InputDecoration(
+                  suffixIcon: PasswordSuffixIconButton(
+                    onPressed: notifier.switchObscureText,
+                    obscureText: state.obscureText,
                   ),
                 ),
               ),
-              const SizedBox(width: 32.0),
-              Expanded(
-                child: FormItem(
-                  label: '名前',
-                  child: TextFormField(
-                    controller: _firstNameController,
-                    decoration: const InputDecoration(hintText: '太郎'),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(height: 32.0),
-          FormItem(
-            label: '学校名',
-            child: Stack(
-              alignment: Alignment.center,
+            ),
+            const SizedBox(height: 32.0),
+            Row(
               children: [
-                TextFormField(
-                  enabled: false,
-                  decoration: const InputDecoration(
-                    disabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        color: Colors.grey,
-                      ),
+                Expanded(
+                  key: _lastNameKey,
+                  child: FormItem(
+                    label: '苗字',
+                    child: TextFormField(
+                      controller: _lastNameController,
+                      decoration: const InputDecoration(hintText: '山田'),
                     ),
                   ),
                 ),
-                DropdownButton(
-                  items: [
-                    for (School school in state.schools)
-                      DropdownMenuItem(
-                        value: school,
-                        child: Text(school.name),
-                      ),
-                  ],
-                  value: state.selectedSchool,
-                  isExpanded: true,
-                  padding: const EdgeInsets.all(16.0),
-                  onChanged: (value) => notifier.onChangedSchool(value),
+                const SizedBox(width: 32.0),
+                Expanded(
+                  child: FormItem(
+                    label: '名前',
+                    child: TextFormField(
+                      controller: _firstNameController,
+                      decoration: const InputDecoration(hintText: '太郎'),
+                    ),
+                  ),
                 ),
               ],
             ),
-          ),
-          const SizedBox(height: 32.0),
-          FormItem(
-            label: '区分（$allUserTypeText）',
-            child: SizedBox(
-              width: state.userTypeFieldWidth,
+            const SizedBox(height: 32.0),
+            FormItem(
+              label: '学校名',
               child: Stack(
                 alignment: Alignment.center,
                 children: [
@@ -171,28 +141,63 @@ class SignUpPageState extends ConsumerState<SignUpPage> {
                   ),
                   DropdownButton(
                     items: [
-                      for (UserType userType in UserType.values)
+                      for (School school in state.schools)
                         DropdownMenuItem(
-                          value: userType,
-                          child: Text(userType.displayText),
+                          value: school,
+                          child: Text(school.name),
                         ),
                     ],
-                    value: state.userType,
+                    value: state.selectedSchool,
                     isExpanded: true,
                     padding: const EdgeInsets.all(16.0),
-                    onChanged: (value) => notifier.onChangedUserType(value),
+                    onChanged: (value) => notifier.onChangedSchool(value),
                   ),
                 ],
               ),
             ),
-          ),
-          const SizedBox(height: 64.0),
-          ElevatedButton(
-            onPressed: _onPressedSignUp,
-            child: const Text('登録'),
-          ),
-          SizedBox(height: keyboardHeight),
-        ],
+            const SizedBox(height: 32.0),
+            FormItem(
+              label: '区分（$allUserTypeText）',
+              child: SizedBox(
+                width: state.userTypeFieldWidth,
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    TextFormField(
+                      enabled: false,
+                      decoration: const InputDecoration(
+                        disabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                    ),
+                    DropdownButton(
+                      items: [
+                        for (UserType userType in UserType.values)
+                          DropdownMenuItem(
+                            value: userType,
+                            child: Text(userType.displayText),
+                          ),
+                      ],
+                      value: state.userType,
+                      isExpanded: true,
+                      padding: const EdgeInsets.all(16.0),
+                      onChanged: (value) => notifier.onChangedUserType(value),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 64.0),
+            ElevatedButton(
+              onPressed: _onPressedSignUp,
+              child: const Text('登録'),
+            ),
+            SizedBox(height: keyboardHeight),
+          ],
+        ),
       ),
     );
   }
