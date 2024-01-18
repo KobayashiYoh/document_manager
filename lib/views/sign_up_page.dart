322ce8ebd9d299b818bf20fbc6c5464c540af1c6
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-01-18 16:09:06 +0900

    不要なContainerを削除

--- lib/views/sign_up_page.dart
+++ lib/views/sign_up_page.dart
@@ -62,59 +62,92 @@ class SignUpPageState extends ConsumerState<SignUpPage> {
       appBar: AppBar(
         title: const Text('新規ユーザー登録'),
       ),
-      body: Container(
-        child: ListView(
-          padding: const EdgeInsets.all(32.0),
-          children: [
-            FormItem(
-              label: 'メールアドレス',
-              child: TextFormField(
-                controller: _emailController,
-                decoration: const InputDecoration(
-                  hintText: 'email@example.com',
+      body: ListView(
+        padding: const EdgeInsets.all(32.0),
+        children: [
+          FormItem(
+            label: 'メールアドレス',
+            child: TextFormField(
+              controller: _emailController,
+              decoration: const InputDecoration(
+                hintText: 'email@example.com',
+              ),
+            ),
+          ),
+          const SizedBox(height: 32.0),
+          FormItem(
+            label: 'パスワード',
+            child: TextFormField(
+              controller: _passwordController,
+              obscureText: state.obscureText,
+              decoration: InputDecoration(
+                suffixIcon: PasswordSuffixIconButton(
+                  onPressed: notifier.switchObscureText,
+                  obscureText: state.obscureText,
                 ),
               ),
             ),
-            const SizedBox(height: 32.0),
-            FormItem(
-              label: 'パスワード',
-              child: TextFormField(
-                controller: _passwordController,
-                obscureText: state.obscureText,
-                decoration: InputDecoration(
-                  suffixIcon: PasswordSuffixIconButton(
-                    onPressed: notifier.switchObscureText,
-                    obscureText: state.obscureText,
+          ),
+          const SizedBox(height: 32.0),
+          Row(
+            children: [
+              Expanded(
+                key: _lastNameKey,
+                child: FormItem(
+                  label: '苗字',
+                  child: TextFormField(
+                    decoration: const InputDecoration(hintText: '山田'),
                   ),
                 ),
               ),
-            ),
-            const SizedBox(height: 32.0),
-            Row(
+              const SizedBox(width: 32.0),
+              Expanded(
+                child: FormItem(
+                  label: '名前',
+                  child: TextFormField(
+                    decoration: const InputDecoration(hintText: '太郎'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: 32.0),
+          FormItem(
+            label: '学校名',
+            child: Stack(
+              alignment: Alignment.center,
               children: [
-                Expanded(
-                  key: _lastNameKey,
-                  child: FormItem(
-                    label: '苗字',
-                    child: TextFormField(
-                      decoration: const InputDecoration(hintText: '山田'),
+                TextFormField(
+                  enabled: false,
+                  decoration: const InputDecoration(
+                    disabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(
+                        color: Colors.grey,
+                      ),
                     ),
                   ),
                 ),
-                const SizedBox(width: 32.0),
-                Expanded(
-                  child: FormItem(
-                    label: '名前',
-                    child: TextFormField(
-                      decoration: const InputDecoration(hintText: '太郎'),
-                    ),
-                  ),
+                DropdownButton(
+                  items: [
+                    for (School school in state.schools)
+                      DropdownMenuItem(
+                        value: school,
+                        child: Text(school.name),
+                      ),
+                  ],
+                  value: state.selectedSchool,
+                  isExpanded: true,
+                  padding: const EdgeInsets.all(16.0),
+                  onChanged: (value) => notifier.onChangedSchool(value),
                 ),
               ],
             ),
-            const SizedBox(height: 32.0),
-            FormItem(
-              label: '学校名',
+          ),
+          const SizedBox(height: 32.0),
+          FormItem(
+            label: '区分（$allUserTypeText）',
+            child: SizedBox(
+              width: state.userTypeFieldWidth,
               child: Stack(
                 alignment: Alignment.center,
                 children: [
@@ -130,63 +163,28 @@ class SignUpPageState extends ConsumerState<SignUpPage> {
                   ),
                   DropdownButton(
                     items: [
-                      for (School school in state.schools)
+                      for (UserType userType in UserType.values)
                         DropdownMenuItem(
-                          value: school,
-                          child: Text(school.name),
+                          value: userType,
+                          child: Text(userType.displayText),
                         ),
                     ],
-                    value: state.selectedSchool,
+                    value: state.userType,
                     isExpanded: true,
                     padding: const EdgeInsets.all(16.0),
-                    onChanged: (value) => notifier.onChangedSchool(value),
+                    onChanged: (value) => notifier.onChangedUserType(value),
                   ),
                 ],
               ),
             ),
-            const SizedBox(height: 32.0),
-            FormItem(
-              label: '区分（$allUserTypeText）',
-              child: SizedBox(
-                width: state.userTypeFieldWidth,
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    TextFormField(
-                      enabled: false,
-                      decoration: const InputDecoration(
-                        disabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                            color: Colors.grey,
-                          ),
-                        ),
-                      ),
-                    ),
-                    DropdownButton(
-                      items: [
-                        for (UserType userType in UserType.values)
-                          DropdownMenuItem(
-                            value: userType,
-                            child: Text(userType.displayText),
-                          ),
-                      ],
-                      value: state.userType,
-                      isExpanded: true,
-                      padding: const EdgeInsets.all(16.0),
-                      onChanged: (value) => notifier.onChangedUserType(value),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(height: 64.0),
-            ElevatedButton(
-              onPressed: _onPressedSignUp,
-              child: const Text('登録'),
-            ),
-            SizedBox(height: keyboardHeight),
-          ],
-        ),
+          ),
+          const SizedBox(height: 64.0),
+          ElevatedButton(
+            onPressed: _onPressedSignUp,
+            child: const Text('登録'),
+          ),
+          SizedBox(height: keyboardHeight),
+        ],
       ),
     );
   }
