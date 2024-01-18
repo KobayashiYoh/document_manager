a97f5e2a433499cf7f4e09451a30688565a8b4cd
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-01-18 22:07:20 +0900

    サインイン中のユーザーを表すlib/providers/signed_in_user_notifier.dart

---
+++ lib/providers/signed_in_user_notifier.dart
@@ -0,0 +1,16 @@
+import 'package:document_manager/models/user.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final signedInUserProvider = StateNotifierProvider<SignedInUserNotifier, User?>(
+  (ref) => SignedInUserNotifier(),
+);
+
+class SignedInUserNotifier extends StateNotifier<User?> {
+  SignedInUserNotifier() : super(null);
+
+  bool get isSignedIn => state != null;
+
+  void setSignedInUser(User user) {
+    state = user;
+  }
+}
