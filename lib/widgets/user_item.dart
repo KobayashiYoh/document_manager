cd12422d84b068404a69310727a4e4fde3da436b
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-01-19 18:55:06 +0900

    UserItemクラスを追加

---
+++ lib/widgets/user_item.dart
@@ -0,0 +1,40 @@
+import 'package:document_manager/models/user.dart';
+import 'package:document_manager/models/user_type.dart';
+import 'package:document_manager/widgets/circle_icon_image.dart';
+import 'package:flutter/material.dart';
+
+class UserItem extends StatelessWidget {
+  const UserItem({Key? key, required this.user}) : super(key: key);
+
+  final User user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(vertical: 16.0),
+      margin: const EdgeInsets.symmetric(horizontal: 16.0),
+      decoration: const BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            color: Colors.grey,
+          ),
+        ),
+      ),
+      child: Row(
+        children: [
+          CircleIconImage(
+            imageUrl: user.iconImageUrl,
+            errorImagePath: 'assets/images/default_user.png',
+          ),
+          const SizedBox(width: 8.0),
+          Text(
+            '${user.lastName} ${user.firstName}（${user.userType.displayText}）',
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
