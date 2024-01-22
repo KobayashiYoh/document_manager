3d84b647497f4613a1feac2b12668574059869cb
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-01-22 15:13:28 +0900

    初期化のタイミングを修正

--- lib/providers/chat_notifier.dart
+++ lib/providers/chat_notifier.dart
@@ -1,6 +1,4 @@
 import 'package:document_manager/models/chat_state.dart';
-import 'package:document_manager/models/school.dart';
-import 'package:document_manager/models/user.dart';
 import 'package:document_manager/providers/signed_in_school_notifier.dart';
 import 'package:document_manager/providers/signed_in_user_notifier.dart';
 import 'package:document_manager/providers/users_notifier.dart';
@@ -13,9 +11,7 @@ final chatProvider = StateNotifierProvider.autoDispose<ChatNotifier, ChatState>(
 );
 
 class ChatNotifier extends StateNotifier<ChatState> {
-  ChatNotifier(this.ref) : super(kDefaultChatState) {
-    initialize();
-  }
+  ChatNotifier(this.ref) : super(kDefaultChatState);
 
   final Ref ref;
 
@@ -25,32 +21,6 @@ class ChatNotifier extends StateNotifier<ChatState> {
 
   void setError(bool value) {
     state = state.copyWith(hasError: value);
-  }
-
-  Future<void> initialize() async {
-    setLoading(true);
-    String signedInUserId;
-    User signedInUser;
-    List<User> users;
-    School school;
-    try {
-      signedInUserId = await SecureStorageRepository.readUserId() ?? '';
-      signedInUser = await FirestoreRepository.getUser(signedInUserId);
-      users = await FirestoreRepository.getUsers();
-      school = await FirestoreRepository.getSchool(signedInUser.schoolId);
-    } catch (e) {
-      setError(true);
-      rethrow;
-    } finally {
-      setLoading(false);
-    }
-    ref.read(signedInUserProvider.notifier).setSignedInUser(signedInUser);
-    ref.read(usersProvider.notifier).setUsers(users);
-    ref.read(signedInSchoolProvider.notifier).setSignedInSchool(school);
-    FirestoreRepository.initilezed(
-      schoolId: signedInUser.schoolId,
-      userId: signedInUserId,
-    );
   }
 
   Future<void> signOut() async {
