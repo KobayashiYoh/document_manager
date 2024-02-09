76c488283fa33c6c4d7534d70efe65b94bd81c74
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-02-09 17:15:19 +0900

    教師のみ管理者画面を開けるように修正

--- lib/widgets/bottom_navigation.dart
+++ lib/widgets/bottom_navigation.dart
@@ -1,4 +1,5 @@
 import 'package:document_manager/constants/app_colors.dart';
+import 'package:document_manager/models/user.dart';
 import 'package:document_manager/pages/admin_page.dart';
 import 'package:document_manager/pages/chat_page.dart';
 import 'package:document_manager/pages/document_page.dart';
@@ -6,6 +7,7 @@ import 'package:document_manager/pages/home_page.dart';
 import 'package:document_manager/pages/loading_view.dart';
 import 'package:document_manager/pages/my_page.dart';
 import 'package:document_manager/providers/bottom_navigation_notifier.dart';
+import 'package:document_manager/providers/signed_in_user_notifier.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -30,6 +32,7 @@ class HomeViewState extends ConsumerState<BottomNavigation> {
   Widget build(BuildContext context) {
     final state = ref.watch(bottomNavigationProvider);
     final notifier = ref.read(bottomNavigationProvider.notifier);
+    final signInUser = ref.watch(signedInUserProvider);
     return Scaffold(
       body: state.isLoading
           ? const LoadingView()
@@ -37,27 +40,28 @@ class HomeViewState extends ConsumerState<BottomNavigation> {
               child: _widgetOptions.elementAt(state.selectedIndex),
             ),
       bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
             icon: Icon(Icons.home_outlined),
             label: 'ホーム',
           ),
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             icon: Icon(Icons.chat_bubble_outline),
             label: 'チャット',
           ),
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             icon: Icon(CupertinoIcons.doc),
             label: 'プリント',
           ),
-          BottomNavigationBarItem(
+          const BottomNavigationBarItem(
             icon: Icon(Icons.person_outline),
             label: 'マイページ',
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.admin_panel_settings_outlined),
-            label: '管理者',
-          ),
+          if (signInUser != null && signInUser.isAdmin)
+            const BottomNavigationBarItem(
+              icon: Icon(Icons.admin_panel_settings_outlined),
+              label: '管理者',
+            ),
         ],
         currentIndex: state.selectedIndex,
         elevation: 8.0,
