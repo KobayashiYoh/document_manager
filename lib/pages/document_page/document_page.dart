f91997d9a98ae1f46b52e09dc529925cae47e1fb
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-02-11 21:09:28 +0900

    チャット画面にプリントの画像つき投稿を表示

--- lib/pages/document_page/document_page.dart
+++ lib/pages/document_page/document_page.dart
@@ -1,3 +1,7 @@
+import 'package:document_manager/models/post.dart';
+import 'package:document_manager/models/user.dart';
+import 'package:document_manager/repository/firestore_repository.dart';
+import 'package:document_manager/widgets/post_item.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -15,8 +19,30 @@ class DocumentPageState extends ConsumerState<DocumentPage> {
       appBar: AppBar(
         title: const Text('プリント'),
       ),
-      body: const Center(
-        child: Text('プリント'),
+      body: StreamBuilder(
+        stream: FirestoreRepository.documentsSnapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.data == null) {
+            return const SizedBox.shrink();
+          }
+          return ListView.builder(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            itemCount: snapshot.data!.docs.length,
+            itemBuilder: (context, index) {
+              final List<Post> posts = snapshot.data!.docs
+                  .map((doc) => Post.fromJson(doc.data()))
+                  .toList();
+              final post = posts[index];
+              return PostItem(
+                post: post,
+                user: kDefaultUser,
+                signedInUserId: kDefaultUser.id,
+                onLongPressCheck: () {},
+                onPressedCheck: () {},
+              );
+            },
+          );
+        },
       ),
     );
   }
