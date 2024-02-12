a992e967d0a0a90cd088b865ee55ee833a736c85
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-02-12 13:43:53 +0900

    PostSearchBarを各画面に配置

--- lib/pages/document_page/document_page.dart
+++ lib/pages/document_page/document_page.dart
@@ -1,6 +1,7 @@
 import 'package:document_manager/models/post.dart';
 import 'package:document_manager/pages/document_page/document_item.dart';
 import 'package:document_manager/repository/firestore_repository.dart';
+import 'package:document_manager/widgets/post_search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -18,32 +19,43 @@ class DocumentPageState extends ConsumerState<DocumentPage> {
       appBar: AppBar(
         title: const Text('プリント'),
       ),
-      body: StreamBuilder(
-        stream: FirestoreRepository.documentsSnapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.data == null) {
-            return const SizedBox.shrink();
-          }
-          return GridView.builder(
-            itemCount: snapshot.data!.docs.length,
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 3,
-              mainAxisSpacing: 4.0,
-              crossAxisSpacing: 4.0,
+      body: Column(
+        children: [
+          PostSearchBar(
+            searchTextController: TextEditingController(),
+            onSubmitted: (value) {},
+            onPressedClear: () {},
+          ),
+          Expanded(
+            child: StreamBuilder(
+              stream: FirestoreRepository.documentsSnapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.data == null) {
+                  return const SizedBox.shrink();
+                }
+                return GridView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 4.0,
+                    crossAxisSpacing: 4.0,
+                  ),
+                  itemBuilder: (context, index) {
+                    final List<Post> posts = snapshot.data!.docs
+                        .map((doc) => Post.fromJson(doc.data()))
+                        .toList();
+                    final post = posts[index];
+                    return DocumentItem(
+                      post: post,
+                      onLongPressCheck: () {},
+                      onPressedCheck: () {},
+                    );
+                  },
+                );
+              },
             ),
-            itemBuilder: (context, index) {
-              final List<Post> posts = snapshot.data!.docs
-                  .map((doc) => Post.fromJson(doc.data()))
-                  .toList();
-              final post = posts[index];
-              return DocumentItem(
-                post: post,
-                onLongPressCheck: () {},
-                onPressedCheck: () {},
-              );
-            },
-          );
-        },
+          ),
+        ],
       ),
     );
   }
