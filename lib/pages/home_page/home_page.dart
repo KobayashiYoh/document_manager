3ee2db69ed04a67602d5415e5a14d58368aa1afa
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-02-15 14:59:42 +0900

    ホーム画面に学校名を表示

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:document_manager/providers/signed_in_school_notifier.dart';
 import 'package:document_manager/providers/users_notifier.dart';
 import 'package:document_manager/widgets/user_item.dart';
 import 'package:flutter/material.dart';
@@ -15,15 +16,35 @@ class HomePageState extends ConsumerState<HomePage> {
   @override
   Widget build(BuildContext context) {
     final users = ref.watch(usersProvider);
+    final school = ref.watch(signedInSchoolProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('ホーム'),
       ),
-      body: Center(
-        child: ListView.builder(
-            padding: EdgeInsets.symmetric(horizontal: 16.w),
-            itemCount: users.length,
-            itemBuilder: (context, index) => UserItem(user: users[index])),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            if (school != null)
+              Container(
+                padding: const EdgeInsets.only(left: 16.0, top: 8.0),
+                child: Text(
+                  school.name,
+                  style: const TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ListView.builder(
+              padding: EdgeInsets.symmetric(horizontal: 16.w),
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: users.length,
+              itemBuilder: (context, index) => UserItem(user: users[index]),
+            ),
+          ],
+        ),
       ),
     );
   }
