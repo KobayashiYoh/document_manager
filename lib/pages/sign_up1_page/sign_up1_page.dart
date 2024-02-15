e230d2332942941cadeac9fa3ff2ba05c82914e1
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-02-15 22:37:34 +0900

    サインアップ画面1/2のUI修正

--- lib/pages/sign_up1_page/sign_up1_page.dart
+++ lib/pages/sign_up1_page/sign_up1_page.dart
@@ -1,6 +1,3 @@
-import 'package:document_manager/models/gender.dart';
-import 'package:document_manager/models/school.dart';
-import 'package:document_manager/models/user_type.dart';
 import 'package:document_manager/providers/sign_up1_notifier.dart';
 import 'package:document_manager/widgets/bottom_navigation.dart';
 import 'package:document_manager/widgets/form_item.dart';
@@ -8,6 +5,7 @@ import 'package:document_manager/widgets/loading_view.dart';
 import 'package:document_manager/widgets/password_suffix_icon_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SignUp1Page extends ConsumerStatefulWidget {
   const SignUp1Page({super.key});
@@ -21,7 +19,6 @@ class SignUp1PageState extends ConsumerState<SignUp1Page> {
   final _passwordController = TextEditingController();
   final _lastNameController = TextEditingController();
   final _firstNameController = TextEditingController();
-  final _lastNameKey = GlobalKey();
 
   Future<void> _onPressedSignUp() async {
     final notifier = ref.read(signUp1Provider.notifier);
@@ -59,181 +56,51 @@ class SignUp1PageState extends ConsumerState<SignUp1Page> {
           Scaffold(
             resizeToAvoidBottomInset: false,
             appBar: AppBar(
-              title: const Text('新規ユーザー登録'),
+              title: const Text('新規ユーザー登録 1/2'),
             ),
             body: AutofillGroup(
-              child: ListView(
-                padding: const EdgeInsets.all(32.0),
-                children: [
-                  FormItem(
-                    label: 'メールアドレス',
-                    child: TextFormField(
-                      controller: _emailController,
-                      keyboardType: TextInputType.emailAddress,
-                      autofillHints: const <String>[AutofillHints.email],
-                      decoration: const InputDecoration(
-                        hintText: 'email@example.com',
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 32.0),
-                  FormItem(
-                    label: 'パスワード',
-                    child: TextFormField(
-                      controller: _passwordController,
-                      keyboardType: TextInputType.visiblePassword,
-                      autofillHints: const <String>[AutofillHints.password],
-                      obscureText: state.obscureText,
-                      decoration: InputDecoration(
-                        suffixIcon: PasswordSuffixIconButton(
-                          onPressed: notifier.switchObscureText,
-                          obscureText: state.obscureText,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 32.0),
-                  Row(
+              child: SingleChildScrollView(
+                child: Container(
+                  padding: const EdgeInsets.all(32.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
                     children: [
-                      Expanded(
-                        key: _lastNameKey,
-                        child: FormItem(
-                          label: '姓',
-                          child: TextFormField(
-                            controller: _lastNameController,
-                            autofillHints: const <String>[
-                              AutofillHints.familyName,
-                            ],
-                            decoration: const InputDecoration(hintText: '山田'),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(width: 32.0),
-                      Expanded(
-                        child: FormItem(
-                          label: '名',
-                          child: TextFormField(
-                            controller: _firstNameController,
-                            autofillHints: const <String>[
-                              AutofillHints.givenName,
-                            ],
-                            decoration: const InputDecoration(hintText: '太郎'),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 32.0),
-                  FormItem(
-                    label: '学校名',
-                    child: Stack(
-                      alignment: Alignment.center,
-                      children: [
-                        TextFormField(
-                          enabled: false,
+                      FormItem(
+                        label: 'メールアドレス',
+                        child: TextFormField(
+                          controller: _emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          autofillHints: const <String>[AutofillHints.email],
                           decoration: const InputDecoration(
-                            disabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                color: Colors.grey,
-                              ),
+                            hintText: 'email@example.com',
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 32.h),
+                      FormItem(
+                        label: 'パスワード',
+                        child: TextFormField(
+                          controller: _passwordController,
+                          keyboardType: TextInputType.visiblePassword,
+                          autofillHints: const <String>[AutofillHints.password],
+                          obscureText: state.obscureText,
+                          decoration: InputDecoration(
+                            suffixIcon: PasswordSuffixIconButton(
+                              onPressed: notifier.switchObscureText,
+                              obscureText: state.obscureText,
                             ),
                           ),
                         ),
-                        DropdownButton(
-                          items: [
-                            for (School school in state.schools)
-                              DropdownMenuItem(
-                                value: school,
-                                child: Text(school.name),
-                              ),
-                          ],
-                          value: state.school,
-                          isExpanded: true,
-                          padding: const EdgeInsets.all(16.0),
-                          onChanged: notifier.onChangedSchool,
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 32.0),
-                  Row(
-                    children: [
-                      Expanded(
-                        child: FormItem(
-                          label: 'ユーザーの種類',
-                          child: Stack(
-                            alignment: Alignment.center,
-                            children: [
-                              TextFormField(
-                                enabled: false,
-                                decoration: const InputDecoration(
-                                  disabledBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(
-                                      color: Colors.grey,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              DropdownButton(
-                                items: [
-                                  for (UserType userType in UserType.values)
-                                    DropdownMenuItem(
-                                      value: userType,
-                                      child: Text(userType.displayText),
-                                    ),
-                                ],
-                                value: state.userType,
-                                isExpanded: true,
-                                padding: const EdgeInsets.all(16.0),
-                                onChanged: notifier.onChangedUserType,
-                              ),
-                            ],
-                          ),
-                        ),
                       ),
-                      const SizedBox(width: 32.0),
-                      Expanded(
-                        child: FormItem(
-                          label: '性別',
-                          child: Stack(
-                            alignment: Alignment.center,
-                            children: [
-                              TextFormField(
-                                enabled: false,
-                                decoration: const InputDecoration(
-                                  disabledBorder: OutlineInputBorder(
-                                    borderSide: BorderSide(
-                                      color: Colors.grey,
-                                    ),
-                                  ),
-                                ),
-                              ),
-                              DropdownButton(
-                                items: [
-                                  for (Gender gender in Gender.values)
-                                    DropdownMenuItem(
-                                      value: gender,
-                                      child: Text(gender.displayText),
-                                    ),
-                                ],
-                                value: state.gender,
-                                isExpanded: true,
-                                padding: const EdgeInsets.all(16.0),
-                                onChanged: notifier.onChangedGender,
-                              ),
-                            ],
-                          ),
-                        ),
+                      SizedBox(height: 320.h),
+                      ElevatedButton(
+                        onPressed: _onPressedSignUp,
+                        child: const Text('次へ'),
                       ),
+                      SizedBox(height: keyboardHeight),
                     ],
                   ),
-                  const SizedBox(height: 64.0),
-                  ElevatedButton(
-                    onPressed: _onPressedSignUp,
-                    child: const Text('登録'),
-                  ),
-                  SizedBox(height: keyboardHeight),
-                ],
+                ),
               ),
             ),
           ),
