33f544ca359697b97837027194cf205c4e08eca1
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2024-02-15 23:58:14 +0900

    ChatPageにローディングビューを表示

--- lib/pages/chat_page/chat_page.dart
+++ lib/pages/chat_page/chat_page.dart
@@ -4,6 +4,7 @@ import 'package:document_manager/pages/chat_room_page/channel_item.dart';
 import 'package:document_manager/pages/chat_room_page/chat_room_page.dart';
 import 'package:document_manager/providers/chat_notifier.dart';
 import 'package:document_manager/repository/firestore_repository.dart';
+import 'package:document_manager/widgets/loading_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -18,64 +19,62 @@ class SignUpPageState extends ConsumerState<ChatPage> {
   @override
   Widget build(BuildContext context) {
     final state = ref.watch(chatProvider);
-    if (state.isLoading) {
-      return const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    }
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('チャット'),
-      ),
-      body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Stack(
-            alignment: Alignment.bottomCenter,
-            children: [
-              StreamBuilder(
-                stream: FirestoreRepository.channelSnapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.data == null) {
-                    return const SizedBox.shrink();
-                  }
-                  return ListView.builder(
-                    itemCount: snapshot.data!.docs.length,
-                    itemBuilder: (context, index) {
-                      final List<Channel> channels = snapshot.data!.docs
-                          .map((doc) => Channel.fromJson(doc.data()))
-                          .toList();
-                      return ChannelItem(
-                        channel: channels[index],
-                        onTap: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  ChatRoomPage(channel: channels[index]),
-                            ),
+    return Stack(
+      children: [
+        Scaffold(
+          appBar: AppBar(
+            title: const Text('チャット'),
+          ),
+          body: SafeArea(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Stack(
+                alignment: Alignment.bottomCenter,
+                children: [
+                  StreamBuilder(
+                    stream: FirestoreRepository.channelSnapshots(),
+                    builder: (context, snapshot) {
+                      if (snapshot.data == null) {
+                        return const SizedBox.shrink();
+                      }
+                      return ListView.builder(
+                        itemCount: snapshot.data!.docs.length,
+                        itemBuilder: (context, index) {
+                          final List<Channel> channels = snapshot.data!.docs
+                              .map((doc) => Channel.fromJson(doc.data()))
+                              .toList();
+                          return ChannelItem(
+                            channel: channels[index],
+                            onTap: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) =>
+                                      ChatRoomPage(channel: channels[index]),
+                                ),
+                              );
+                            },
                           );
                         },
                       );
                     },
-                  );
-                },
+                  ),
+                ],
               ),
-            ],
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const DebugPage(),
+                ),
+              );
+            },
+            child: const Icon(Icons.developer_mode),
           ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (context) => const DebugPage(),
-            ),
-          );
-        },
-        child: const Icon(Icons.developer_mode),
-      ),
+        if (state.isLoading) const LoadingView(),
+      ],
     );
   }
 }
